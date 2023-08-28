ab9fdec8cae2f53010274421a603f33da1652dd7
“bridge-demo” <mariobv@kyndryl.com>
2023-08-28 09:07:07 -0600

    tf templates / adding azure simple vm

--- terraform-templates/azure/simpleVM/simpleVM/main.tf
+++ terraform-templates/azure/simpleVM/simpleVM/main.tf
@@ -1,87 +1,82 @@
 provider "azurerm" {
   features {}
-  
 }
-
-terraform {
-  required_version = "1.0.9"
-} 
-variable "resource_group" {
-  default = "terraformTest3"
-  type = string
-  description = "resource group"
-}
-
 
 
 resource "azurerm_resource_group" "example" {
-  name     = var.resource_group
-  location = "East US"
+  name     = var.group_name
+  location = var.group_location
 }
 
+variable "group_name" {
+  default = "example-resource"
+  type    = string
+}
+
+variable "group_location" {
+default = "East US"
+type    = string
+}
+
+
+
 resource "azurerm_virtual_network" "example" {
-  name                = "myVnettf"
+  name                = "example-network"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
 
 resource "azurerm_subnet" "example" {
-  name                 = "mySubnet"
+  name                 = "internal"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_public_ip" "example" {
-  name                = "myPublicIP"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  allocation_method  = "Dynamic"
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "example" {
-  name                = "myNIC"
+  name                = "example-nic"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
-    name                          = "myNICConfig"
+    name                          = "internal"
     subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
   }
 }
 
-resource "azurerm_virtual_machine" "example" {
-  name                  = "myVM"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.example.id]
 
-  vm_size = "Standard_DS1_v2"
+variable "virtualmachine_name" {
+  default = "example-machine"
+  type    = string
+}
 
-  storage_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
+variable "virtualmachine_size" {
+  default = "Standard_F2"
+  type    = string
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = var.virtualmachine_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = var.virtualmachine_size
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+  disable_password_authentication = false
+  admin_password = "Mcmprocks123"
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
     version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "myOsDisk"
-    caching           = "ReadWrite"
-    create_option    = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  os_profile {
-    computer_name  = "myvm"
-    admin_username = "adminuser"
-    admin_password = "P@ssw0rd123!"
-  }
-
-  os_profile_windows_config {
-    enable_automatic_upgrades = true
   }
 }
